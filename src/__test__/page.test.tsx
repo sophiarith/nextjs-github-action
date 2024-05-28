69e718804a8ab0e988abca443746b47886da8912
Sophiarith <sophiarith@gmail.com>
2024-05-28 22:25:20 +0700

    update test

---
+++ src/__test__/page.test.tsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from '../app/page'
+import { describe, it } from 'node:test'
+ 
+describe('Page', () => {
+  it('renders a heading', () => {
+    render(<Page />)
+ 
+    const heading = screen.getByRole('heading', { level: 1 })
+ 
+    expect(heading).toBeInTheDocument()
+  })
+})
+
+
